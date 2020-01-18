9f04435cc071ffe5e2c96b3017da99b4ac146339
Abdullahi <Mohamed>
2020-01-17 19:37:32 -0500

    AM: React basic setup.

---
+++ hackathon/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h>NKU | Random Access</h>
+    </div>
+  );
+}
+
+export default App;
